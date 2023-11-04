0fb1c5748ec2a6eb27838a98d9b1303354a5e550
Your Name <you@example.com>
2023-11-04 16:08:30 +0700

    up

--- module2/src/_15_Exception_Debug/exercise/_e1/Test.java
+++ module2/src/_15_Exception_Debug/exercise/_e1/Test.java
@@ -9,13 +9,20 @@ public class Test {
         int b= scanner.nextInt();
         int c= scanner.nextInt();
         try {
-            Triangle triangle = new Triangle(a, b, c);
-            System.out.println(triangle.sumofside());
+            triangle(a,b,c);
+
         } catch (IllegalTriangleException e) {
             e.printStackTrace();
         }
         System.out.println("finished");
 
 
+    }
+    public static void triangle(int a,int b,int c)throws IllegalTriangleException
+    {
+        if(a<=0||b<=0||c<=0||a+b<=c||b+c<=a||a+c<=b)
+        {
+            throw new IllegalTriangleException("three a,b,c cannot is side of a triangle");
+        }
     }
 }
